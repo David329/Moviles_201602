37554e4cc6910d3225b5f9ed1287eb6abe3f28d7
David Silva <davidsilva3290@gmail.com>
2016-09-17 16:16:56 -0500

    Se conecto el index con Login, Agrege el VM Login teniendo como objeto enterprise como entidad(atributos) y cliente(metodos) de los WS

--- Moviles_201602/Proyecto/GuianClient/GuianClient/Controllers/HomeController.cs
+++ Moviles_201602/Proyecto/GuianClient/GuianClient/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using GuianClient.ViewModel;
 
 namespace GuianClient.Controllers
 {
@@ -16,6 +17,20 @@ namespace GuianClient.Controllers
         public ActionResult Login()
         {
             return View();
-        }       
+        }
+
+        [HttpPost]
+        public ActionResult AddEnterprise(LoginViewModel ObjViewModel)
+        {
+            try
+            {
+                ObjViewModel.AddEnterprise(ObjViewModel.ObjEnterprise);
+                return RedirectToAction("Login", "Home");
+            }
+            catch (Exception)
+            {
+                return RedirectToAction("Login", "Home");
+            }
+        }
     }
 }
---
+++ Moviles_201602/Proyecto/GuianClient/GuianClient/ViewModel/LoginViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GuianClient.ViewModel
+{
+    public class LoginViewModel
+    {
+        private ServiceEnterprise.ServiceEnterpriseClient service;
+
+        public ServiceEnterprise.enterprise ObjEnterprise { get; set; }
+
+        public void AddEnterprise(ServiceEnterprise.enterprise obj)
+        {
+            service = new ServiceEnterprise.ServiceEnterpriseClient();
+            service.addEnterprise(obj);
+        }
+    }
+}
